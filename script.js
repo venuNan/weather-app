ffd5f00e67f4bdc37ab2259f3b2aec1d2a4f7d8a
venuNan <venureddy3808@gmail.com>
2024-06-27 00:53:18 +0530

    modified script file and added some png's

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ async function weather() {
     errorMessageElement.style.display = "none";
 
     try {
+
+        //
         const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=us&include=days&key=RC5YBMR5N93FWUMF338LY4YBV&contentType=json`;
         const response = await fetch(url);
 
@@ -23,29 +25,89 @@ async function weather() {
         const data = await response.json();
         console.log(data);
         
+
+        // the below line is to display the full address of the user
         const fullAddress = data.resolvedAddress;
-        const pressure = data.days[0].pressure;
-        const windSpeed = data.days[0].windspeed;
-        const visibility = data.days[0].visibility;
-        const maxTemp = data.days[0].tempmax;
-        const minTemp = data.days[0].tempmin;
-        const temp = data.days[0].temp;
-        const todayCondition = data.days[0].conditions;
-        
-
-        console.log("Full Address:", fullAddress);
-        console.log("Pressure:", pressure);
-        console.log("Wind Speed:", windSpeed);
-        console.log("Visibility:", visibility);
-        console.log("Max Temp:", maxTemp);
-        console.log("Min Temp:", minTemp);
-        console.log("temp :", temp);
-        console.log("today condition:", todayCondition);
-
         document.querySelector(".location #address").textContent = fullAddress;
-        document.getElementById("celcius").innerHTML = `${((temp-32)*5/9).toFixed(0)} <sup>o</sup>C`;
+
+        // the follwing if else statements is to display the weather icon for today
+        const icon = data.days[0].icon.toLowerCase();
+        const desc = data.days[0].description.toLowerCase();
+        console.log(icon);
+        const weathericon = document.querySelector(".information .today-weather .today-temperature #icon");
+        if(icon.includes("clear")){
+            weathericon.style.backgroundImage = "url(sun.png)";
+        }
+        else if(icon.includes("rain") && desc.includes("cloudy") && desc.includes("storm")){
+            weathericon.style.backgroundImage = "url(cloud-rain-storm.png)";
+        }
+        else if(icon.includes("cloudy") && desc.includes("storm")){
+            weathericon.style.backgroundImage = "url(cloud.storm.png)";
+        }
+        else if(icon.includes("partly") && icon.includes("cloudy")){
+            weathericon.style.backgroundImage = "url(partly-cloudy.png)";
+        }
+        else if(icon.includes("cloudy")){
+            weathericon.style.backgroundImage = "url(cloudy.png)";
+        }
+        else if(icon.includes("rain")){
+            weathericon.style.backgroundImage = "url(cloud-rain.png)";
+        }
         
-        document.querySelector(".information .today-weather .today-temperature #condition").textContent = todayCondition;
+
+        // the below two lines is to play the description of the weather and todays temperature
+        const description = data.days[0].description;
+        const temp = data.days[0].temp;
+        document.getElementById("celcius").innerHTML = `${((temp-32)*5/9).toFixed(0)} <sup>o</sup>C`;
+        document.querySelector(".information .today-weather .today-temperature #desc").textContent = description;
+
+        // the next two lines is to display today's date
+        const today = new Date();
+        const options = { day: '2-digit', month: 'long', year: 'numeric' };
+        const formattedDate = today.toLocaleDateString('en-US', options);
+        document.querySelector(".information .today-weather .today-details #date #value").textContent = formattedDate;
+
+        // the below lines until else condition is to display todays details of date,humidity,windspeed,winddirection,visiblity,pressure
+        const pressure = data.days[0].pressure;
+        document.querySelector(".information .today-weather .today-details #pressure #value").textContent = pressure +" mb";
+        const windSpeed = data.days[0].windspeed;
+        document.querySelector(".information .today-weather .today-details #windspeed #value").textContent = windSpeed+" km/h";
+        const visibility = data.days[0].visibility;
+        document.querySelector(".information .today-weather .today-details #visibility #value").textContent = visibility +" km";
+        const humidity = data.days[0].humidity;
+        document.querySelector(".information .today-weather .today-details #humidity #value").textContent = humidity.toFixed(1)+" %";
+        const windDir = data.days[0].winddir;
+        if (windDir >= 67.5 && windDir <= 112.5) {
+            document.querySelector(".information .today-weather .today-details #wind-direction #value").textContent = "East";
+        } else if (windDir >= 157.5 && windDir <= 202.5) {
+            document.querySelector(".information .today-weather .today-details #wind-direction #value").textContent = "South";
+        } else if (windDir >= 247.5 && windDir <= 292.5) {
+            document.querySelector(".information .today-weather .today-details #wind-direction #value").textContent = "West";
+        } else {
+            document.querySelector(".information .today-weather .today-details #wind-direction #value").textContent = "North";
+        }
+
+        const formattedDates = [];
+        for (let i = 0; i < 10; i++) {
+            const date = new Date(today);
+            date.setDate(today.getDate() + i);
+            const formattedDate = date.toLocaleDateString('en-US', options);
+            formattedDates.push(formattedDate);
+        }
+
+        document.querySelector(".information .tenday-weather #day1 #date").textContent = formattedDates[0];
+        document.querySelector(".information .tenday-weather #day2 #date").textContent = formattedDates[1];
+        document.querySelector(".information .tenday-weather #day3 #date").textContent = formattedDates[2];
+        document.querySelector(".information .tenday-weather #day4 #date").textContent = formattedDates[3];
+        document.querySelector(".information .tenday-weather #day5 #date").textContent = formattedDates[4];
+        document.querySelector(".information .tenday-weather #day6 #date").textContent = formattedDates[5];
+        document.querySelector(".information .tenday-weather #day7 #date").textContent = formattedDates[6];
+        document.querySelector(".information .tenday-weather #day8 #date").textContent = formattedDates[7];
+        document.querySelector(".information .tenday-weather #day9 #date").textContent = formattedDates[8];
+        document.querySelector(".information .tenday-weather #day10 #date").textContent = formattedDates[9];
+
+
+
     } catch (error) {
         errorMessageElement.style.display = "block";
         console.error('Error fetching weather data:', error);
