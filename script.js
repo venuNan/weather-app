f92091df921b0b999433e3ee4ba35a7ed27a6b64
venuNan <venureddy3808@gmail.com>
2024-06-27 14:15:25 +0530

    complated building the app by adding extra pngs for weather icons and lets is there are any error by using it for one week

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ document.getElementById("inplace").addEventListener("input",function(){
     document.getElementById("error-message").style.display = "none";
 });
 
-async function weather() {
-    const place = document.getElementById("inplace").value.trim(); 
+function default_function(){
+    const place = "mangalam";
+    weather(place);
+
+}
+
+window.onload = default_function;
+function user_function(){
+    const place = document.getElementById("inplace").value.trim();
+    weather(place);
+}
+async function weather(place) { 
     if (place === "") {
         document.getElementById("error-message").style.display = "block";
         return;
@@ -14,8 +24,8 @@ async function weather() {
 
     try {
 
-        //
-        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=us&include=days&key=RC5YBMR5N93FWUMF338LY4YBV&contentType=json`;
+        console.log(place);
+        const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=metric&include=days%2Chours&key=RC5YBMR5N93FWUMF338LY4YBV&contentType=json`;
         const response = await fetch(url);
 
         if (!response.ok) {
@@ -23,7 +33,6 @@ async function weather() {
         }
 
         const data = await response.json();
-        console.log(data);
         
 
         // the below line is to display the full address of the user
@@ -33,7 +42,6 @@ async function weather() {
         // the follwing if else statements is to display the weather icon for today
         const icon = data.days[0].icon.toLowerCase();
         const desc = data.days[0].description.toLowerCase();
-        console.log(icon);
         const weathericon = document.querySelector(".information .today-weather .today-temperature #icon");
         if(icon.includes("clear")){
             weathericon.style.backgroundImage = "url(sun.png)";
@@ -58,7 +66,7 @@ async function weather() {
         // the below two lines is to play the description of the weather and todays temperature
         const description = data.days[0].description;
         const temp = data.days[0].temp;
-        document.getElementById("celcius").innerHTML = `${((temp-32)*5/9).toFixed(0)} <sup>o</sup>C`;
+        document.getElementById("celcius").innerHTML = `${(temp).toFixed(0)} <sup>o</sup>C`;
         document.querySelector(".information .today-weather .today-temperature #desc").textContent = description;
 
         // the next two lines is to display today's date
@@ -86,7 +94,7 @@ async function weather() {
         } else {
             document.querySelector(".information .today-weather .today-details #wind-direction #value").textContent = "North";
         }
-
+        // the following for loop and fromattedDates variable is to dispaly the dates of tenday forecast
         const formattedDates = [];
         for (let i = 0; i < 10; i++) {
             const date = new Date(today);
@@ -94,19 +102,93 @@ async function weather() {
             const formattedDate = date.toLocaleDateString('en-US', options);
             formattedDates.push(formattedDate);
         }
-
-        document.querySelector(".information .tenday-weather #day1 #date").textContent = formattedDates[0];
-        document.querySelector(".information .tenday-weather #day2 #date").textContent = formattedDates[1];
-        document.querySelector(".information .tenday-weather #day3 #date").textContent = formattedDates[2];
-        document.querySelector(".information .tenday-weather #day4 #date").textContent = formattedDates[3];
-        document.querySelector(".information .tenday-weather #day5 #date").textContent = formattedDates[4];
-        document.querySelector(".information .tenday-weather #day6 #date").textContent = formattedDates[5];
-        document.querySelector(".information .tenday-weather #day7 #date").textContent = formattedDates[6];
-        document.querySelector(".information .tenday-weather #day8 #date").textContent = formattedDates[7];
-        document.querySelector(".information .tenday-weather #day9 #date").textContent = formattedDates[8];
-        document.querySelector(".information .tenday-weather #day10 #date").textContent = formattedDates[9];
-
-
+        
+        // the following for loop and ten_weather_variable is to diaply th e dates and weather icons of tenday forecast
+        const ten_day_weather = document.querySelector(".tenday-weather1 .tenday-weather");
+        ten_day_weather.innerHTML = "";
+        for (let i=0;i<10;i++){
+            // 
+            const day = document.createElement("div");
+            day.className = "day";
+            day.id = `day${i+1}`;
+            const date = document.createElement("div");
+            date.id = "date";
+            date.innerText = formattedDates[i];
+            day.append(date);
+            const temp_symbol = document.createElement("div");
+            temp_symbol.className = "temp-symbol";
+            const temp = document.createElement("div");
+            temp.id = "temp";
+            temp.innerHTML = `${(((data.days[i].tempmin)).toFixed(0))}<sup>o</sup>C  &nbsp/&nbsp  ${(((data.days[i].tempmax)).toFixed(0))}<sup>o</sup>C`;
+            temp_symbol.append(temp);
+            const symbol = document.createElement("div");
+            symbol.id = "symbol";
+            const icon1 = data.days[i].icon.toLowerCase();
+            const desc1 = data.days[i].description.toLowerCase();
+            if(icon1.includes("clear")){
+                symbol.style.backgroundImage = "url(sun.png)";
+            }
+            else if(icon1.includes("rain") && desc1.includes("cloudy") && desc1.includes("storm")){
+                symbol.style.backgroundImage = "url(cloud-rain-storm.png)";
+            }
+            else if(icon1.includes("cloudy") && desc1.includes("storm")){
+                symbol.style.backgroundImage = "url(cloud-storm.png)";
+            }
+            else if(icon1.includes("partly") && icon1.includes("cloudy")){
+                symbol.style.backgroundImage = "url(partly-cloudy.png)";
+            }
+            else if(icon1.includes("cloudy") && desc1.includes("rain")){
+                symbol.style.backgroundImage = "url(cloudy-rains.png)";
+            }
+            else if(icon1.includes("cloudy")){
+                symbol.style.backgroundImage = "url(cloudy.png)";
+            }
+            else if(icon1.includes("rain")){
+                symbol.style.backgroundImage = "url(cloud-rain.png)";
+            }
+            temp_symbol.append(symbol);
+            day.append(temp_symbol);
+            ten_day_weather.append(day);        
+        }
+        
+        // the following for loop  is to display today's hourly weather
+        const todayweather = data.days[0].hours;        
+        for(let i=0;i<24;i++){
+            const icon2 = data.days[0].hours[i].icon.toLowerCase();
+            const desc2 = data.days[0].hours[i].conditions.toLowerCase();
+            const datetime = data.days[0].hours[i].datetime.split(":")[0];
+            console.log(datetime)
+            if(icon2.includes("clear") && ((datetime>=0 && datetime<=5) || (datetime>=17 && datetime<=23))){
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(night.png)";
+            }
+            else if(icon2.includes("clear")){
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(sun.png)";
+            }
+            else if(icon2.includes("rain") && desc2.includes("cloudy") && desc2.includes("storm")){
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(cloud-rain-storm.png)";
+            }
+            else if(icon2.includes("cloudy") && desc2.includes("storm")){
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(cloud-storm.png)";
+            }
+            else if(icon2.includes("partly") && icon2.includes("cloudy") && ((datetime>=0 && datetime<=5) || (datetime>=17 && datetime<=23))){
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(cloud-night.png)";
+            }
+            else if(icon2.includes("partly") && icon2.includes("cloudy")){
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(partly-cloudy.png)";
+            }
+            else if(icon2.includes("cloudy") && desc2.includes("rain") && ((datetime>=0 && datetime<=5) && (datetime>=19 && datetime<=23))){//
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(cloudy-rainy-night.png)";
+            }
+            else if(icon2.includes("cloudy") && desc2.includes("rain")){
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(cloudy-rains.png)";
+            }
+            else if(icon2.includes("cloudy")){
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(cloudy.png)";
+            }
+            else if(icon2.includes("rain")){
+                document.querySelector(`.hourly-weather1 .hourly-weather .container #hour${i+1} #image`).style.backgroundImage = "url(cloud-rain.png)";
+            }
+        }
 
     } catch (error) {
         errorMessageElement.style.display = "block";
